fedf98773160f26cc240f3aed1fbd7be3f0bb257
Aaron Sproul <aaron@liveramp.com>
2018-09-12 14:26:43 -0700

    Format examples using terraform fmt

--- examples/simple_project/main.tf
+++ examples/simple_project/main.tf
@@ -21,16 +21,16 @@ module "test-vpc-module" {
 
   subnets = [
     {
-      subnet_name           = "subnet-01"
-      subnet_ip             = "10.10.10.0/24"
-      subnet_region         = "us-west1"
+      subnet_name   = "subnet-01"
+      subnet_ip     = "10.10.10.0/24"
+      subnet_region = "us-west1"
     },
     {
       subnet_name           = "subnet-02"
       subnet_ip             = "10.10.20.0/24"
       subnet_region         = "us-west1"
       subnet_private_access = true
-      subnet_flow_logs      = true 
+      subnet_flow_logs      = true
     },
   ]
 
