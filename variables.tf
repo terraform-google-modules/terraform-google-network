d4695e9253e67b78a671db6b09fe1a0292bc214d
Aaron Lane <aarondrewlane@gmail.com>
2019-07-04 16:33:31 -0400

    Fix type of secondary_ranges variable

--- variables.tf
+++ variables.tf
@@ -40,7 +40,7 @@ variable "subnets" {
 }
 
 variable "secondary_ranges" {
-  type        = list(map(object))
+  type        = map(list(object({ range_name = string, ip_cidr_range = string })))
   description = "Secondary ranges that will be used in some of the subnets"
 }
 
