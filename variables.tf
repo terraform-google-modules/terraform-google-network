3dabbd1bf4f469715c45a7bb75228eeea4f9502f
Ken Evensen <kdevensen@google.com>
2019-07-22 15:21:51 -0400

    Explicitly set type as bool

--- variables.tf
+++ variables.tf
@@ -63,6 +63,7 @@ variable "description" {
 }
 
 variable "auto_create_subnetworks" {
+  type        = bool
   description = "When set to true, the network is created in 'auto subnet mode' and it will create a subnet for each region automatically across the 10.128.0.0/9 address range. When set to false, the network is created in 'custom subnet mode' so the user can explicitly connect subnetwork resources."
-  default     = "false"
+  default     = false
 }
