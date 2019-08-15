fd46ec056e37bac40fd22124183bc788b734107c
Morgante Pell <morgantep@google.com>
2019-08-15 18:37:55 -0400

    Remove old simulated_ci_env

--- test/setup/variables.tf
+++ test/setup/variables.tf
@@ -14,10 +14,13 @@
  * limitations under the License.
  */
 variable "org_id" {
+    description = "The numeric organization id"
 }
 
 variable "folder_id" {
+    description = "The folder to deploy in"
 }
 
 variable "billing_account" {
+    description = "The billing account id associated with the project, e.g. XXXXXX-YYYYYY-ZZZZZZ"
 }
