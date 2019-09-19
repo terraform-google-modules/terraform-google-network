f1590db1a746c1910f535f64f2b43266f82aad27
Tim Hartmann <tfhartmann@gmail.com>
2019-09-19 16:05:42 -0400

    fix: fixing incorrect outputs

--- outputs.tf
+++ outputs.tf
@@ -30,37 +30,37 @@ output "svpc_host_project_id" {
 }
 
 output "subnets_names" {
-  value       = values(google_compute_subnetwork.subnetwork)[*].name
+  value       = google_compute_subnetwork.subnetwork[*].name
   description = "The names of the subnets being created"
 }
 
 output "subnets_ips" {
-  value       = values(google_compute_subnetwork.subnetwork)[*].ip_cidr_range
+  value       = google_compute_subnetwork.subnetwork[*].ip_cidr_range
   description = "The IPs and CIDRs of the subnets being created"
 }
 
 output "subnets_self_links" {
-  value       = values(google_compute_subnetwork.subnetwork)[*].self_link
+  value       = google_compute_subnetwork.subnetwork[*].self_link
   description = "The self-links of subnets being created"
 }
 
 output "subnets_regions" {
-  value       = values(google_compute_subnetwork.subnetwork)[*].region
+  value       = google_compute_subnetwork.subnetwork[*].region
   description = "The region where the subnets will be created"
 }
 
 output "subnets_private_access" {
-  value       = values(google_compute_subnetwork.subnetwork)[*].private_ip_google_access
+  value       = google_compute_subnetwork.subnetwork[*].private_ip_google_access
   description = "Whether the subnets will have access to Google API's without a public IP"
 }
 
 output "subnets_flow_logs" {
-  value       = values(google_compute_subnetwork.subnetwork)[*].enable_flow_logs
+  value       = google_compute_subnetwork.subnetwork[*].enable_flow_logs
   description = "Whether the subnets will have VPC flow logs enabled"
 }
 
 output "subnets_secondary_ranges" {
-  value       = values(google_compute_subnetwork.subnetwork)[*].secondary_ip_range
+  value       = google_compute_subnetwork.subnetwork[*].secondary_ip_range
   description = "The secondary ranges associated with these subnets"
 }
 
