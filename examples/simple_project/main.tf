a9d2dbf3d00c92481dde86de83c6f08e35119c2b
Josh Hill <josh.hill@cloudsolutions.co.uk>
2019-11-15 15:48:00 +0000

    Updates simple-project example
    Adds `subnet_flow_logs` to subnet 3 due to new condition.

--- examples/simple_project/main.tf
+++ examples/simple_project/main.tf
@@ -50,6 +50,7 @@ module "test-vpc-module" {
       subnet_name               = "${local.subnet_03}"
       subnet_ip                 = "10.10.30.0/24"
       subnet_region             = "us-west1"
+      subnet_flow_logs          = "true"
       subnet_flow_logs_interval = "INTERVAL_10_MIN"
       subnet_flow_logs_sampling = 0.7
       subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
