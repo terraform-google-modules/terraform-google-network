fec8285517c21c5f84662b30dc9df834c70bc3ab
Taylor Ludwig <taylorludwig@gmail.com>
2019-11-25 15:32:23 -0800

    Revert "specify provider versions for each module for terraform init/validate"
    This reverts commit 3a3a408e39ce648e629c0dd63081a17f0f9c8c87.

--- test/setup/versions.tf
+++ test/setup/versions.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019 Google LLC
+ * Copyright 2018 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
  */
 
 terraform {
-  required_version = "~> 0.12.0"
+  required_version = ">= 0.12"
 }
 
 provider "google" {
--- versions.tf
+++ versions.tf
@@ -17,7 +17,3 @@
 terraform {
   required_version = "~> 0.12.0"
 }
-
-provider "google" {
-  version = "~> 2.18.0"
-}
