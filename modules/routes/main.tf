d567d892fd42007b0a267ac6668ef3630b9a8320
Taylor Ludwig <taylorludwig@gmail.com>
2019-11-25 16:18:07 -0800

    default rotue values to null so google_compute_route defaults are used

--- modules/routes/main.tf
+++ modules/routes/main.tf
@@ -31,15 +31,15 @@ resource "google_compute_route" "route" {
   network = var.network_name
 
   name                   = each.key
-  description            = lookup(each.value, "description", "")
+  description            = lookup(each.value, "description", null)
   tags                   = compact(split(",", lookup(each.value, "tags", "")))
-  dest_range             = lookup(each.value, "destination_range", "")
+  dest_range             = lookup(each.value, "destination_range", null)
   next_hop_gateway       = lookup(each.value, "next_hop_internet", "false") == "true" ? "default-internet-gateway" : ""
-  next_hop_ip            = lookup(each.value, "next_hop_ip", "")
-  next_hop_instance      = lookup(each.value, "next_hop_instance", "")
-  next_hop_instance_zone = lookup(each.value, "next_hop_instance_zone", "")
-  next_hop_vpn_tunnel    = lookup(each.value, "next_hop_vpn_tunnel", "")
-  priority               = lookup(each.value, "priority", "1000")
+  next_hop_ip            = lookup(each.value, "next_hop_ip", null)
+  next_hop_instance      = lookup(each.value, "next_hop_instance", null)
+  next_hop_instance_zone = lookup(each.value, "next_hop_instance_zone", null)
+  next_hop_vpn_tunnel    = lookup(each.value, "next_hop_vpn_tunnel", null)
+  priority               = lookup(each.value, "priority",null)
 
   depends_on = [
     var.network,
