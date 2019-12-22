59608e28ea4c57b85a3b52b6eb1c608497bc37b4
bharathkkb <bharathkrishnakb@gmail.com>
2019-12-22 13:43:10 -0600

    fix test

--- examples/simple_project/main.tf
+++ examples/simple_project/main.tf
@@ -51,8 +51,8 @@ module "test-vpc-module" {
       subnet_ip                 = "10.10.30.0/24"
       subnet_region             = "us-west1"
       subnet_flow_logs          = "true"
-      subnet_flow_logs_interval = "INTERVAL_15_MIN"
-      subnet_flow_logs_sampling = 0.9
+      subnet_flow_logs_interval = "INTERVAL_10_MIN"
+      subnet_flow_logs_sampling = 0.7
       subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
     }
   ]
