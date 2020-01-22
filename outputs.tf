36996e9345b7f60a3ca8a2dcd114eece5881443e
Adrian Gierakowski <agierakowski@gmail.com>
2020-01-22 20:01:29 +0000

    add description for the 'subnets' output

--- outputs.tf
+++ outputs.tf
@@ -70,5 +70,6 @@ output "route_names" {
 }
 
 output "subnets" {
-  value = module.subnets.subnets
+  value       = module.subnets.subnets
+  description = "A map with keys of form subnet_region/subnet_name and values being the outputs of the google_compute_subnetwork resources used to create corresponding subnets."
 }
