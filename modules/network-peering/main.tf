7e5f5ab9fa4f19b4995ae24c9db1234dbdbcb487
Morgante Pell <morgantep@google.com>
2020-05-14 14:04:53 -0400

    fix: Remove quoted references (#183)

--- modules/network-peering/main.tf
+++ modules/network-peering/main.tf
@@ -20,7 +20,7 @@ locals {
 }
 
 resource "google_compute_network_peering" "local_network_peering" {
-  provider             = "google-beta"
+  provider             = google-beta
   name                 = "${var.prefix}-${local.local_network_name}-${local.peer_network_name}"
   network              = var.local_network
   peer_network         = var.peer_network
@@ -31,7 +31,7 @@ resource "google_compute_network_peering" "local_network_peering" {
 }
 
 resource "google_compute_network_peering" "peer_network_peering" {
-  provider             = "google-beta"
+  provider             = google-beta
   name                 = "${var.prefix}-${local.peer_network_name}-${local.local_network_name}"
   network              = var.peer_network
   peer_network         = var.local_network
