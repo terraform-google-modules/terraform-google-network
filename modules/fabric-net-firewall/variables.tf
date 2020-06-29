12ce5fb0852d91360061d0635e33f335f7705fb3
Rafael Jesus <rafaelljesus86@gmail.com>
2020-06-29 14:54:02 +0200

    allow to pass target_tags to the default firewalls
    fix typo
    
    use the doc generator instead of adding readme manually

--- modules/fabric-net-firewall/variables.tf
+++ modules/fabric-net-firewall/variables.tf
@@ -32,6 +32,11 @@ variable "internal_ranges" {
   default     = []
 }
 
+variable "internal_target_tags" {
+  description = "List of target tags for intra-VPC rules."
+  default     = []
+}
+
 variable "internal_allow" {
   description = "Allow rules for internal ranges."
   default = [
@@ -56,14 +61,29 @@ variable "ssh_source_ranges" {
   default     = ["0.0.0.0/0"]
 }
 
+variable "ssh_target_tags" {
+  description = "List of target tags for tag-based SSH rule, defaults to ssh."
+  default     = ["ssh"]
+}
+
 variable "http_source_ranges" {
   description = "List of IP CIDR ranges for tag-based HTTP rule, defaults to 0.0.0.0/0."
   default     = ["0.0.0.0/0"]
 }
 
+variable "http_target_tags" {
+  description = "List of target tags for tag-based HTTP rule, defaults to http-server."
+  default     = ["http-server"]
+}
+
 variable "https_source_ranges" {
   description = "List of IP CIDR ranges for tag-based HTTPS rule, defaults to 0.0.0.0/0."
   default     = ["0.0.0.0/0"]
+}
+
+variable "https_target_tags" {
+  description = "List of target tags for tag-based HTTPS rule, defaults to https-server."
+  default     = ["https-server"]
 }
 
 variable "custom_rules" {
