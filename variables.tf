01b37b593cc864f2cd0ad4746c9a6ce8e0a8c1db
Bharath KKB <bharathkrishnakb@gmail.com>
2021-01-21 21:35:09 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@
  */
 
 variable "project_id" {
-  description = "The ID of the project where this VPC will be created"
+  description = "The ID test of the project where this VPC will be created"
 }
 
 variable "network_name" {
