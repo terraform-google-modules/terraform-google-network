0718d9a1fe2fd8647f965eef05517c7f6009400f
Betsy Lichtenberg <betsyl@google.com>
2021-03-17 14:18:05 -0700

    ran terraform fmt

--- examples/simple_project/main.tf
+++ examples/simple_project/main.tf
@@ -31,10 +31,10 @@ locals {
 # [START vpc_custom_create]
 module "test-vpc-module" {
   source       = "terraform-google-modules/terraform-google-network/google"
-  version = "~> 3.2.0"
+  version      = "~> 3.2.0"
   project_id   = var.project_id
   network_name = var.network_name
-  mtu = 1460
+  mtu          = 1460
 
   subnets = [
     {
