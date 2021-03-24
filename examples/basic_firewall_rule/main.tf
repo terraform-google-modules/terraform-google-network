3a0d8bcacd881e94f6eb6ea155bf2b24fe7a61bb
Betsy Lichtenberg <betsyl@google.com>
2021-03-24 13:42:43 -0700

    Added comment for project ID

--- examples/basic_firewall_rule/main.tf
+++ examples/basic_firewall_rule/main.tf
@@ -25,7 +25,7 @@ provider "google" {
 
 # [START firewall_create]
 resource "google_compute_firewall" "rules" {
-  project     = var.project_id
+  project     = var.project_id # Replace this with your project ID in quotes
   name        = "my-firewall-rule"
   network     = "default"
   description = "Creates firewall rule targeting tagged instances"
