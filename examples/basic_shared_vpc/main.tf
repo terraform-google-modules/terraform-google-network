74c6bd867f4391d0be6148c4b14d7fb2569ddd69
Betsy Lichtenberg <betsyl@google.com>
2021-04-12 19:17:58 -0700

    Fixed formatting

--- examples/basic_shared_vpc/main.tf
+++ examples/basic_shared_vpc/main.tf
@@ -31,9 +31,8 @@ resource "google_compute_shared_vpc_host_project" "host" {
 
 # [START vpc_shared_vpc_service_project_attach]
 resource "google_compute_shared_vpc_service_project" "service1" {
-  host_project    = google_compute_shared_vpc_host_project.host.project  
+  host_project    = google_compute_shared_vpc_host_project.host.project
   service_project = var.service_project # Replace this with your service project ID in quotes
 }
 # [END vpc_shared_vpc_service_project_attach]
-
 
