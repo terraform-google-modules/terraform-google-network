b42f31d3fa25fc0efe530250a13a971364b7fd19
Betsy Lichtenberg <betsyl@google.com>
2021-04-12 19:08:27 -0700

    Added basic Terraform example for Shared VPC

---
+++ examples/basic_shared_vpc/main.tf
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "google" {
+  version = "~> 3.45.0"
+}
+
+# [START vpc_shared_vpc_host_project_enable]
+resource "google_compute_shared_vpc_host_project" "host" {
+  project = var.project # Replace this with your host project ID in quotes
+}
+# [END vpc_shared_vpc_host_project_enable]
+
+# [START vpc_shared_vpc_service_project_attach]
+resource "google_compute_shared_vpc_service_project" "service1" {
+  host_project    = google_compute_shared_vpc_host_project.host.project  
+  service_project = var.service_project # Replace this with your service project ID in quotes
+}
+# [END vpc_shared_vpc_service_project_attach]
+
+
