90576466b15eb2c7889e8227f558a85779f5f489
Betsy Lichtenberg <betsyl@google.com>
2021-04-14 15:14:32 -0700

    Removed trailing whitespace

--- basic_vpc_network_peering/main.tf
+++ basic_vpc_network_peering/main.tf
@@ -27,7 +27,7 @@ provider "google" {
 module "peering1" {
   source        = "terraform-google-modules/network/google//modules/network-peering"
   local_network = var.local_network # Replace with self link to VPC network "foobar" in quotes
-  peer_network  = var.peer_network  # Replace with self link to VPC network "other" in quotes 
+  peer_network  = var.peer_network  # Replace with self link to VPC network "other" in quotes
 }
 
 module "peering2" {
