a46c5152396c9c71b077053aa39686be3ab44351
Betsy Lichtenberg <betsyl@google.com>
2021-04-20 13:20:13 -0700

    Pinned basic VPC peering example to a version

--- examples/basic_vpc_peering/main.tf
+++ examples/basic_vpc_peering/main.tf
@@ -26,6 +26,7 @@ provider "google" {
 # [START vpc_peering_create]
 module "peering1" {
   source        = "terraform-google-modules/network/google//modules/network-peering"
+  version       = "~> 3.2.1"
   local_network = var.local_network # Replace with self link to VPC network "foobar" in quotes
   peer_network  = var.peer_network  # Replace with self link to VPC network "other" in quotes
 }
