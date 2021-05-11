95daff5e79324ebf214c396fe67ce5f997ca0a30
betsy-lichtenberg <betsyl@google.com>
2021-05-10 18:59:29 -0700

    chore: Added Terraform example for firewall rule with logging (#293)

---
+++ examples/firewall_logging/main.tf
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "google" {
+  version = "~> 3.45.0"
+}
+
+# [START vpc_firewall_create]
+resource "google_compute_firewall" "rules" {
+  project     = var.project_id # Replace this with your project ID in quotes
+  name        = "my-firewall-rule"
+  network     = "default"
+  description = "Creates firewall rule targeting tagged instances"
+
+  log_config {
+    metadata = "INCLUDE_ALL_METADATA"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000"]
+  }
+  target_tags = ["web"]
+}
+# [END vpc_firewall_create]
