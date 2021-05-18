08fddcb0c92ac32e15bf38e4a255150154c1298c
tdigangi <tdigangi@google.com>
2021-05-18 18:53:32 -0400

    Adding itegration test iam permissions and improving ip_cidr_range example

--- examples/submodule_vpc_serverless_connector/main.tf
+++ examples/submodule_vpc_serverless_connector/main.tf
@@ -42,15 +42,23 @@ module "serverless-connector" {
   source     = "../../modules/vpc-serverless-connector-beta"
   project_id = var.project_id
   vpc_connectors = [{
-    name   = "central-serverless"
-    region = "us-central1"
-    # Uncomment network & ip_cidr_range then set subnet_name = null to leverage ip_cidr_range
-    # network       = module.test-vpc-module.network_name
-    # ip_cidr_range = "10.10.11.0/28"
+    name        = "central-serverless"
+    region      = "us-central1"
     subnet_name = module.test-vpc-module.subnets["us-central1/serverless-subnet"].name
     # host_project_id = var.host_project_id # Leverage host_project_id if using a shared VPC
     machine_type  = "e2-standard-4"
     min_instances = 2
     max_instances = 7
-  }]
+    }
+    # Uncomment below to leverage ip_cidr_range
+  #   , {
+  #     name          = "central-serverless2"
+  #     region        = "us-central1"
+  #     network       = module.test-vpc-module.network_name
+  #     ip_cidr_range = "10.10.11.0/28"
+  #     subnet_name   = null
+  #     machine_type  = "e2-standard-4"
+  #     min_instances = 2
+  #   max_instances = 7 }
+  # ]
 }
