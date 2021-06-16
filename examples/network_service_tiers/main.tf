97b4b9d5fede8aa5b9a62e55162f61570d7ddb91
mark1000 <vanguard@google.com>
2021-06-16 10:12:55 -0700

    chore: adding instance template and forwarding rule to standard tier (#305)

--- examples/network_service_tiers/main.tf
+++ examples/network_service_tiers/main.tf
@@ -60,3 +60,41 @@ resource "google_compute_instance" "vm" {
   }
 }
 # [END networkservicetiers_vm_create]
+
+# [START networkservicetiers_target_instance_create]
+resource "google_compute_target_instance" "target" {
+  project  = var.project_id # Replace this with your project ID in quotes
+  name     = "target"
+  zone     = "us-east4-c"
+  instance = google_compute_instance.vm.id
+}
+# [END networkservicetiers_target_instance_create]
+
+# [START networkservicetiers_forwarding_rule_create]
+resource "google_compute_forwarding_rule" "target-fr" {
+  project      = var.project_id # Replace this with your project ID in quotes
+  name         = "target-instance-forwarding-rule"
+  region       = "us-east4"
+  target       = google_compute_target_instance.target.id
+  port_range   = "80"
+  network_tier = "STANDARD"
+}
+# [END networkservicetiers_forwarding_rule_create]
+
+# [START networkservicetiers_instance_template_create]
+resource "google_compute_instance_template" "template" {
+  project      = var.project_id # Replace this with your project ID in quotes
+  name         = "template"
+  machine_type = "e2-medium"
+  disk {
+    source_image = "debian-cloud/debian-10"
+    boot         = true
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      network_tier = "STANDARD"
+    }
+  }
+}
+# [END networkservicetiers_instance_template_create]
