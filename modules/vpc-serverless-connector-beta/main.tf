56483b50642e3c3244710dd47693963e4ea65026
Ryan Sinnet <rsinnet@misorobotics.com>
2021-10-05 23:06:58 -0700

    Add max_throughput to vpc-serverless-connector-beta
    Expose the max_throughput argument of the google_vpc_access_connector.

--- modules/vpc-serverless-connector-beta/main.tf
+++ modules/vpc-serverless-connector-beta/main.tf
@@ -31,7 +31,8 @@ resource "google_vpc_access_connector" "connector_beta" {
       project_id = lookup(each.value, "host_project_id", null)
     }
   }
-  machine_type  = lookup(each.value, "machine_type", null)
-  min_instances = lookup(each.value, "min_instances", null)
-  max_instances = lookup(each.value, "max_instances", null)
+  machine_type   = lookup(each.value, "machine_type", null)
+  min_instances  = lookup(each.value, "min_instances", null)
+  max_instances  = lookup(each.value, "max_instances", null)
+  max_throughput = lookup(each.value, "max_throughput", null)
 }
