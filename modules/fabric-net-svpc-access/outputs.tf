8d28367efaacca9c5009e9f955b8e7142f03c089
Nikhil Makhijani <nikhilmakhijani28@hotmail.com>
2021-10-20 11:36:40 +0530

    Replacing count with for_each on google_compute_shared_vpc_service_project to prevent destroy/recreate when the list changes and changing outputs.tf

--- modules/fabric-net-svpc-access/outputs.tf
+++ modules/fabric-net-svpc-access/outputs.tf
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-utput "service_projects" {
+output "service_projects" {
   description = "Project ids of the services with access to all subnets."
   value = {
     for i, k in google_compute_shared_vpc_service_project.projects : i => k.service_project
