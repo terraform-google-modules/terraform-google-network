de1c58217faf336422f352df7c223795a83fbf47
Nikhil Makhijani <nikhilmakhijani28@hotmail.com>
2021-10-21 21:28:07 +0530

    formatting outputs.tf

--- modules/fabric-net-svpc-access/outputs.tf
+++ modules/fabric-net-svpc-access/outputs.tf
@@ -17,6 +17,6 @@
 output "service_projects" {
   description = "Project ids of the services with access to all subnets."
   value = [
-      for i, k in google_compute_shared_vpc_service_project.projects : k.service_project
+    for i, k in google_compute_shared_vpc_service_project.projects : k.service_project
   ]
- }
+}
