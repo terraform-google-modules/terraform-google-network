96929913331c6adc12db17d981920c6903b8b9a8
Jonathan Yu <jawnsy@cpan.org>
2021-11-24 22:30:37 +0000

    bump provider dependency to minimum 3.83

--- modules/subnets/versions.tf
+++ modules/subnets/versions.tf
@@ -20,7 +20,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "<5.0,>= 2.12"
+      version = "<5.0,>= 3.83"
     }
   }
 
--- versions.tf
+++ versions.tf
@@ -19,7 +19,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "< 5.0, >= 2.12"
+      version = "< 5.0, >= 3.83"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
