23e1affb30c95cfbb37c219239e14f8c3f223d9d
Jamie Mitchell <jamiemitchell@gmail.com>
2022-04-21 10:41:10 -0400

    feat: adding logging filter_expr control
    Added filter_expr control to provide control over VPC log filtering

--- examples/simple_project/main.tf
+++ examples/simple_project/main.tf
@@ -49,6 +49,8 @@ module "test-vpc-module" {
       subnet_flow_logs_interval = "INTERVAL_10_MIN"
       subnet_flow_logs_sampling = 0.7
       subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
+      // New
+      subnet_flow_logs_filter = "true"
     }
   ]
 }
