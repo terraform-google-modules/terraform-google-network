9406f83200fdc697dc512365e4fc06e41ca30eb3
Max Portocarrero CI&T <mportocarrero@ciandt.com>
2022-07-20 12:55:59 -0500

    added provider meta google-beta

--- modules/private-service-connect/versions.tf
+++ modules/private-service-connect/versions.tf
@@ -30,4 +30,8 @@ terraform {
   provider_meta "google" {
     module_name = "blueprints/terraform/terraform-google-network:private-service-connect/v5.1.0"
   }
+
+  provider_meta "google-beta" {
+    module_name = "blueprints/terraform/terraform-google-network:private-service-connect/v5.1.0"
+  }
 }
