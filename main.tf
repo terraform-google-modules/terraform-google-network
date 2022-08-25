8dc0eca659b3fbd4bdc05c3389e1988c0a218481
bat251533 <112114084+bat251533@users.noreply.github.com>
2022-08-25 23:11:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,64 +18,64 @@
 	VPC configuration
  *****************************************/
 module "vpc" {
-  source                                 = "./modules/vpc"
-  network_name                           = var.network_name
-  auto_create_subnetworks                = var.auto_create_subnetworks
-  routing_mode                           = var.routing_mode
-  project_id                             = var.project_id
-  description                            = var.description
-  shared_vpc_host                        = var.shared_vpc_host
-  delete_default_internet_gateway_routes = var.delete_default_internet_gateway_routes
-  mtu                                    = var.mtu
-}
+    source  = "terraform-google-modules/network/google"
+    version = "~> 4.0"
 
-/******************************************
-	Subnet configuration
- *****************************************/
-module "subnets" {
-  source           = "./modules/subnets"
-  project_id       = var.project_id
-  network_name     = module.vpc.network_name
-  subnets          = var.subnets
-  secondary_ranges = var.secondary_ranges
-}
+    project_id   = "<PROJECT ID>"
+    network_name = "example-vpc"
+    routing_mode = "GLOBAL"
 
-/******************************************
-	Routes
- *****************************************/
-module "routes" {
-  source            = "./modules/routes"
-  project_id        = var.project_id
-  network_name      = module.vpc.network_name
-  routes            = var.routes
-  module_depends_on = [module.subnets.subnets]
-}
+    subnets = [
+        {
+            subnet_name           = "subnet-01"
+            subnet_ip             = "10.10.10.0/24"
+            subnet_region         = "us-west1"
+        },
+        {
+            subnet_name           = "subnet-02"
+            subnet_ip             = "10.10.20.0/24"
+            subnet_region         = "us-west1"
+            subnet_private_access = "true"
+            subnet_flow_logs      = "true"
+            description           = "This subnet has a description"
+        },
+        {
+            subnet_name               = "subnet-03"
+            subnet_ip                 = "10.10.30.0/24"
+            subnet_region             = "us-west1"
+            subnet_flow_logs          = "true"
+            subnet_flow_logs_interval = "INTERVAL_10_MIN"
+            subnet_flow_logs_sampling = 0.7
+            subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
+        }
+    ]
 
-/******************************************
-	Firewall rules
- *****************************************/
-locals {
-  rules = [
-    for f in var.firewall_rules : {
-      name                    = f.name
-      direction               = f.direction
-      priority                = lookup(f, "priority", null)
-      description             = lookup(f, "description", null)
-      ranges                  = lookup(f, "ranges", null)
-      source_tags             = lookup(f, "source_tags", null)
-      source_service_accounts = lookup(f, "source_service_accounts", null)
-      target_tags             = lookup(f, "target_tags", null)
-      target_service_accounts = lookup(f, "target_service_accounts", null)
-      allow                   = lookup(f, "allow", [])
-      deny                    = lookup(f, "deny", [])
-      log_config              = lookup(f, "log_config", null)
+    secondary_ranges = {
+        subnet-01 = [
+            {
+                range_name    = "subnet-01-secondary-01"
+                ip_cidr_range = "192.168.64.0/24"
+            },
+        ]
+
+        subnet-02 = []
     }
-  ]
-}
 
-module "firewall_rules" {
-  source       = "./modules/firewall-rules"
-  project_id   = var.project_id
-  network_name = module.vpc.network_name
-  rules        = local.rules
+    routes = [
+        {
+            name                   = "egress-internet"
+            description            = "route through IGW to access internet"
+            destination_range      = "0.0.0.0/0"
+            tags                   = "egress-inet"
+            next_hop_internet      = "true"
+        },
+        {
+            name                   = "app-proxy"
+            description            = "route through proxy to reach app"
+            destination_range      = "10.50.10.0/24"
+            tags                   = "app-proxy"
+            next_hop_instance      = "app-proxy-instance"
+            next_hop_instance_zone = "us-west1-a"
+        },
+    ]
 }
