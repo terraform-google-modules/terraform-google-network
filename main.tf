b97d67193f5a7d92f78448d62be023963e3d6554
dennislopes <dennislopes@gmail.com>
2022-10-25 15:30:42 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,19 +30,19 @@ module "test-vpc-module" {
 
   subnets = [
     {
-      subnet_name   = "subnet-01"
+      subnet_name   = "subnet-001"
       subnet_ip     = "10.10.10.0/24"
       subnet_region = "us-west1"
     },
     {
-      subnet_name           = "subnet-02"
+      subnet_name           = "subnet-002"
       subnet_ip             = "10.10.20.0/24"
       subnet_region         = "us-west1"
       subnet_private_access = "true"
       subnet_flow_logs      = "true"
     },
     {
-      subnet_name               = "subnet-03"
+      subnet_name               = "subnet-003"
       subnet_ip                 = "10.10.30.0/24"
       subnet_region             = "us-west1"
       subnet_flow_logs          = "true"
