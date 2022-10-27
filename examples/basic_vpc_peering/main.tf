25299dd864925941271bb772aeba1670801f7dab
Andrew Peabody <andrewpeabody@google.com>
2022-10-27 12:48:09 -0700

    Update main.tf

--- examples/basic_vpc_peering/main.tf
+++ examples/basic_vpc_peering/main.tf
@@ -23,7 +23,7 @@
 # [START vpc_peering_create]
 module "peering1" {
   source        = "terraform-google-modules/network/google//modules/network-peering"
-  version       = "~> 5.2.0"
+  version       = "~> 5.2"
   local_network = var.local_network # Replace with self link to VPC network "foobar" in quotes
   peer_network  = var.peer_network  # Replace with self link to VPC network "other" in quotes
 }
