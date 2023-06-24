2470d6b02a414a2ca1eb23e69c716c6ed4cd007d
Imran Nayer <imrannayer@google.com>
2023-06-24 17:58:35 -0500

    feat: add destination_ranges and source_ranges in firewall rules

--- modules/firewall-rules/main.tf
+++ modules/firewall-rules/main.tf
@@ -14,8 +14,14 @@
  * limitations under the License.
  */
 
+locals {
+  ingress_rules = { for rules in var.ingress_rules : rules.name => merge(rules, { direction = "INGRESS" }) }
+  egress_rules  = { for rules in var.egress_rules : rules.name => merge(rules, { direction = "EGRESS" }) }
+  rules_all     = merge(local.ingress_rules, local.egress_rules)
+}
+
 resource "google_compute_firewall" "rules" {
-  for_each                = { for r in var.rules : r.name => r }
+  for_each                = length(var.rules) > 0 ? { for r in var.rules : r.name => r } : {}
   name                    = each.value.name
   description             = each.value.description
   direction               = each.value.direction
@@ -23,6 +29,45 @@ resource "google_compute_firewall" "rules" {
   project                 = var.project_id
   source_ranges           = each.value.direction == "INGRESS" ? each.value.ranges : null
   destination_ranges      = each.value.direction == "EGRESS" ? each.value.ranges : null
+  source_tags             = each.value.source_tags
+  source_service_accounts = each.value.source_service_accounts
+  target_tags             = each.value.target_tags
+  target_service_accounts = each.value.target_service_accounts
+  priority                = each.value.priority
+
+  dynamic "log_config" {
+    for_each = lookup(each.value, "log_config") == null ? [] : [each.value.log_config]
+    content {
+      metadata = log_config.value.metadata
+    }
+  }
+
+  dynamic "allow" {
+    for_each = lookup(each.value, "allow", [])
+    content {
+      protocol = allow.value.protocol
+      ports    = lookup(allow.value, "ports", null)
+    }
+  }
+
+  dynamic "deny" {
+    for_each = lookup(each.value, "deny", [])
+    content {
+      protocol = deny.value.protocol
+      ports    = lookup(deny.value, "ports", null)
+    }
+  }
+}
+
+resource "google_compute_firewall" "rules_ingress_egress" {
+  for_each                = length(var.rules) > 0 ? {} : local.rules_all
+  name                    = each.value.name
+  description             = each.value.description
+  direction               = each.value.direction
+  network                 = var.network_name
+  project                 = var.project_id
+  source_ranges           = lookup(each.value, "source_ranges", null)
+  destination_ranges      = lookup(each.value, "destination_ranges", null)
   source_tags             = each.value.source_tags
   source_service_accounts = each.value.source_service_accounts
   target_tags             = each.value.target_tags
--- modules/firewall-rules/variables.tf
+++ modules/firewall-rules/variables.tf
@@ -25,26 +25,83 @@ variable "network_name" {
 }
 
 variable "rules" {
-  description = "List of custom rule definitions (refer to variables file for syntax)."
+  description = "This is DEPRICATED and available for backward compatiblity. Use ingress_rules and egress_rules variables. List of custom rule definitions"
   default     = []
   type = list(object({
     name                    = string
-    description             = optional(string)
-    direction               = optional(string)
-    priority                = optional(number)
-    ranges                  = optional(list(string))
+    description             = optional(string, null)
+    direction               = optional(string, "INGRESS")
+    priority                = optional(number, null)
+    ranges                  = optional(list(string), [])
     source_tags             = optional(list(string))
     source_service_accounts = optional(list(string))
     target_tags             = optional(list(string))
     target_service_accounts = optional(list(string))
+
     allow = optional(list(object({
       protocol = string
       ports    = optional(list(string))
-    })))
+    })), [])
     deny = optional(list(object({
       protocol = string
       ports    = optional(list(string))
-    })))
+    })), [])
+    log_config = optional(object({
+      metadata = string
+    }))
+  }))
+}
+
+variable "ingress_rules" {
+  description = "List of ingress rules. This will be ignored if variable 'rules' is non-empty"
+  default     = []
+  type = list(object({
+    name                    = string
+    description             = optional(string, null)
+    priority                = optional(number, null)
+    destination_ranges      = optional(list(string), [])
+    source_ranges           = optional(list(string), [])
+    source_tags             = optional(list(string))
+    source_service_accounts = optional(list(string))
+    target_tags             = optional(list(string))
+    target_service_accounts = optional(list(string))
+
+    allow = optional(list(object({
+      protocol = string
+      ports    = optional(list(string))
+    })), [])
+    deny = optional(list(object({
+      protocol = string
+      ports    = optional(list(string))
+    })), [])
+    log_config = optional(object({
+      metadata = string
+    }))
+  }))
+}
+
+variable "egress_rules" {
+  description = "List of egress rules. This will be ignored if variable 'rules' is non-empty"
+  default     = []
+  type = list(object({
+    name                    = string
+    description             = optional(string, null)
+    priority                = optional(number, null)
+    destination_ranges      = optional(list(string), [])
+    source_ranges           = optional(list(string), [])
+    source_tags             = optional(list(string))
+    source_service_accounts = optional(list(string))
+    target_tags             = optional(list(string))
+    target_service_accounts = optional(list(string))
+
+    allow = optional(list(object({
+      protocol = string
+      ports    = optional(list(string))
+    })), [])
+    deny = optional(list(object({
+      protocol = string
+      ports    = optional(list(string))
+    })), [])
     log_config = optional(object({
       metadata = string
     }))
