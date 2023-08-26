a65c41a8afede4a6cc64d5090c91a6cb25a23f87
Nanda Girindratama <32257758+nandagirin@users.noreply.github.com>
2023-08-27 00:39:56 +0700

    feat: add firewall policy enforcement order (#486)
    Co-authored-by: Imran Nayer <imrannayer@google.com>

--- variables.tf
+++ variables.tf
@@ -178,3 +178,9 @@ variable "egress_rules" {
     }))
   }))
 }
+
+variable "network_firewall_policy_enforcement_order" {
+  type        = string
+  default     = null
+  description = "Set the order that Firewall Rules and Firewall Policies are evaluated. Valid values are `BEFORE_CLASSIC_FIREWALL` and `AFTER_CLASSIC_FIREWALL`. (default null or equivalent to `AFTER_CLASSIC_FIREWALL`)"
+}
--- versions.tf
+++ versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "< 5.0, >= 3.83"
+      version = "< 5.0, >= 4.64"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "< 5.0, >= 3.45"
+      version = "< 5.0, >= 4.64"
     }
   }
 
