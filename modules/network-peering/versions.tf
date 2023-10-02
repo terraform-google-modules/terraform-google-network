2de6e5583a525a785cbac9276753a5110fcb7952
Manuel Morejon <manuel@mmorejon.io>
2023-10-02 20:18:20 +0200

    change version constraint format
    Signed-off-by: Manuel Morejon <manuel@mmorejon.io>

--- modules/network-peering/versions.tf
+++ modules/network-peering/versions.tf
@@ -20,11 +20,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "<5.0,>= 4.65"
+      version = ">= 4.65, <5.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "<5.0,>= 4.65"
+      version = ">= 4.65, <5.0"
     }
     random = {
       source  = "hashicorp/random"
