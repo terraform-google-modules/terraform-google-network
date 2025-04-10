92b5bb88182741cd7430d484dec269adc0bd51b2
Imran Nayer <imrannayer@google.com>
2025-04-10 13:10:52 +0000

    added support for src_networks, src_network_scope, dest_network_scope in hierarchical-firewall-policy

--- modules/hierarchical-firewall-policy/versions.tf
+++ modules/hierarchical-firewall-policy/versions.tf
@@ -20,11 +20,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.64, < 7"
+      version = ">= 6.18, < 7"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 4.64, < 7"
+      version = ">= 6.18, < 7"
     }
   }
 
