924c1b4f242a51e94a27369f66d302f1643f281f
Imran Nayer <imrannayer@google.com>
2025-04-18 21:54:29 +0000

    fixed minor issues

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -18,7 +18,7 @@
 	VPC configuration
  *****************************************/
 resource "google_compute_network" "network" {
-  provider                                  = google-beta  
+  provider                                  = google-beta
   name                                      = var.network_name
   auto_create_subnetworks                   = var.auto_create_subnetworks
   routing_mode                              = var.routing_mode
