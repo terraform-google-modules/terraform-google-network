323f423536de63e0421bde6364e065c8d56fb61c
Swarna Bharathi Mantena <swarna.bharathi1208@gmail.com>
2025-12-26 09:03:57 +0000

    add policy_type variable to network firewall policy

--- modules/network-firewall-policy/variables.tf
+++ modules/network-firewall-policy/variables.tf
@@ -46,6 +46,12 @@ variable "policy_region" {
   default     = null
 }
 
+variable "policy_type" {
+  description = "Policy type is used to determine which resources (networks) the policy can be associated with. A policy can be associated with a network only if the network has the matching policyType in its network profile. Different policy types may support some of the Firewall Rules features. Possible values are: VPC_POLICY, RDMA_ROCE_POLICY."
+  type        = string
+  default     = null
+}
+
 variable "rules" {
   description = "List of Ingress/Egress rules"
   type = list(object({
