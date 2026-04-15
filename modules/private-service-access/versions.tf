52bc733194938aa25bf7006c94b4d4026cd3582b
zubitripathy <70265191+zubitripathy@users.noreply.github.com>
2026-04-15 18:30:05 +0530

    Update copyright year and provider versions in versions.tf

--- modules/private-service-access/versions.tf
+++ modules/private-service-access/versions.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2022 Google LLC
+ * Copyright 2026 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,11 +20,15 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.83, < 8"
+      version = ">= 6.18, < 8"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 6.18, < 8"
     }
   }
 
   provider_meta "google" {
-    module_name = "blueprints/terraform/terraform-google-network:routes/v18.0.0"
+    module_name = "blueprints/terraform/terraform-google-network:private-service-access/v18.0.0"
   }
 }
