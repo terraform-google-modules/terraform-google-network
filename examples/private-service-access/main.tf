1abce4fef75e4889396fac663affe86199ed7af2
zubitripathy <70265191+zubitripathy@users.noreply.github.com>
2026-05-03 21:20:04 +0530

    feat: Implement private services connection module with examples and tests (#692)
    Co-authored-by: krprabhat-eng <krprabhat@google.com>

---
+++ examples/private-service-access/main.tf
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module "vpc" {
+  source       = "../.."
+  project_id   = var.project_id
+  network_name = "example-psa-vpc"
+
+  private_service_access_config = {
+    enable_private_services_connection = true
+    address_name                       = "custom-psa-address"
+    prefix_length                      = 16
+  }
+
+  subnets = [
+    {
+      subnet_name   = "psa-subnet"
+      subnet_ip     = "10.0.0.0/24"
+      subnet_region = "us-central1"
+    }
+  ]
+}
---
+++ modules/private-service-access/variables.tf
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "project_id" {
+  description = "The project ID of the VPC network."
+  type        = string
+}
+
+variable "network_id" {
+  description = "The ID of the VPC network (e.g., google_compute_network.vpc.id)."
+  type        = string
+}
+
+variable "address_name" {
+  description = "The name of the reserved IP range."
+  type        = string
+  default     = "private-ip-address"
+}
+
+variable "prefix_length" {
+  description = "The prefix length of the reserved IP range."
+  type        = number
+  default     = 16
+}
